4fd8f8677085471057757530efc398767cfb23cb
Medipelly Manvith Kumar <m6783321@gmail.com>
2026-05-30 11:45:43 +0530

    Update main.py

--- models/main.py
+++ models/main.py
@@ -1,28 +1,7 @@
-from flask import Flask, request, jsonify
-import joblib
-import pandas as pd
-import os
+from flask import Flask
 
 app = Flask(__name__)
-
-model = joblib.load("github_model.pkl")
 
 @app.route("/")
 def home():
     return {"status": "ok"}
-
-@app.route("/predict", methods=["POST"])
-def predict():
-    data = request.json
-
-    df = pd.DataFrame([data])
-
-    score = model.predict(df)[0]
-
-    return jsonify({
-        "score": round(float(score), 2)
-    })
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
